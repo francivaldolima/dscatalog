acc3f718eef2a1fb875d892a1ac849883cf510fe
francivaldolima <francivaldolimadesousa@gmail.com>
2022-03-23 10:50:42 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.entites.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1l, "Books"));
+        list.add(new Category(2L, "Electonics"));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
